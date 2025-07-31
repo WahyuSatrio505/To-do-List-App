1c8a05f4eecd6fb4d28aaf95805e4b2345d23b0a
wahyusatriowibowo <wahyuusatriowibowo@gmail.com>
2025-07-31 17:48:02 +0700

    wahyuuyuyuyu

--- assets/animasiGSAP.js
+++ assets/animasiGSAP.js
@@ -3,9 +3,26 @@ window.addEventListener("load", function () {
   document.getElementById("preloader").style.display = "none";
 });
 
-  gsap.from(".page", {
+gsap.from(".page", {
+  opacity: 0,
+  y: 50,
+  duration: 1,
+  ease: "power2.out",
+});
+
+window.addEventListener("load", function () {
+  gsap.to("#preloader", {
+    opacity: 0,
+    duration: 1,
+    onComplete: () => {
+      document.getElementById("preloader").style.display = "none";
+    },
+  });
+  // Optionally animasi masuk halaman
+  gsap.from(".main-content", {
     opacity: 0,
     y: 50,
-    duration: 1,
-    ease: "power2.out"
+    duration: 2,
+    delay: 2,
   });
+});
